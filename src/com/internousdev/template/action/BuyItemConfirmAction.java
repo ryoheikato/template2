16b319cb4260c7d07ad40d857e5dd324d324192d
ryohei KATO <ryohei KATO@192.168.1.8>
2017-10-25 16:48:52 +0900

    ファイルコピー

---
+++ src/com/internousdev/template/action/BuyItemConfirmAction.java
@@ -0,0 +1,39 @@
+package com.internousdev.template.action;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.template.dao.BuyItemComplateDAO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BuyItemConfirmAction extends ActionSupport implements SessionAware{
+
+	public Map<String,Object> session;
+
+	private BuyItemComplateDAO buyItemComplateDAO = new BuyItemComplateDAO();
+
+	/**
+	 * 商品購入情報登録メソッド
+	 *
+	 * @author internous
+	 */
+	public String execute() throws SQLException {
+
+		buyItemComplateDAO.buyItemeInfo(
+				session.get("id").toString(),
+				session.get("login_user_id").toString(),
+				session.get("total_price").toString(),
+				session.get("count").toString(),
+				session.get("pay").toString());
+
+		String result = SUCCESS;
+		return result;
+	}
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+}
